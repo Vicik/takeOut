6ea148b9b7b8f4d3875f4a456f431a4fdf31994a
Vicik <sprung@126.com>
2019-03-22 14:53:48 +0800

    修改了退出登录

--- js/add.js
+++ js/add.js
@@ -7,10 +7,12 @@ $(function() {
             $("#userInfo").html(`欢迎回来:${uid}用户 <a href='#' class='navbar-link'>退出登录</a>`);
         }
         $("#header").on("click","a:contains(退出登录)",function(){
+            sessionStorage.removeItem('uid');
             $("#userInfo").html(`
                     <a href="register.html" class="navbar-link">注册</a>
                     <a href="login.html" class="navbar-link">登录</a>
             `);
+
         })
     });
     $('#footer').load('data/footer.php');
